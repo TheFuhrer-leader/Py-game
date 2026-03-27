6bc361bc41ed7b46ec80bd28f1684493782b987d
TheFuhrer-leader <manhdatnguyen0803@gmail.com>
2026-03-26 19:08:48 -0500

    settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all setting for alien invasion"""
+
+    def __init__(self):
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (169, 140, 255)
+
+        self.ship_speed = 1.5
